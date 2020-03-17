0f13189b45c5df78b28fbf17c6c424c24baf9678
janaka <bjchathuranga@gmail.com>
2020-03-17 09:50:35 +0530

    Add sqlplus cli tools

--- install-instantclient.sh
+++ install-instantclient.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 unzip instantclient-basic-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
 unzip instantclient-sdk-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
+unzip instantclient-sqlplus-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
+
 rm -rf /opt/oracle/instantclient
 mv /opt/oracle/instantclient_19_6 /opt/oracle/instantclient
 ln -s /opt/oracle/instantclient/libclntsh.so.19.6 /opt/oracle/instantclient/libclntsh.so
