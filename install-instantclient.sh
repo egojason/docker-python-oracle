1761a0033ed35dea43c8feeb3154904b474c88dc
janaka <bjchathuranga@gmail.com>
2020-04-08 14:01:10 +0530

    Add oracle tools. Ex: expdp

--- install-instantclient.sh
+++ install-instantclient.sh
@@ -2,6 +2,7 @@
 unzip instantclient-basic-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
 unzip instantclient-sdk-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
 unzip instantclient-sqlplus-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
+unzip instantclient-tools-linux.x64-19.6.0.0.0dbru.zip -d /opt/oracle
 
 rm -rf /opt/oracle/instantclient
 mv /opt/oracle/instantclient_19_6 /opt/oracle/instantclient
@@ -14,3 +15,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
 export OCI_HOME="/opt/oracle/instantclient"
 export OCI_LIB_DIR="/opt/oracle/instantclient"
 export OCI_INCLUDE_DIR="/opt/oracle/instantclient/sdk/include"
+export PATH=$PATH:$ORACLE_HOME
